f3f170de94524d384ceb9359243c303fa5fd861b
Kevin Gonzalez <kvngonzalez35@gmail.com>
2023-06-20 18:00:29 -0500

    Creacion de archivos de javascript, y avance con la funcionalidad de la calculadora, hasta el punto de imprimir y borrar numeros.

---
+++ Display.js
@@ -0,0 +1,36 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual){
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+    }
+
+    borrar(){
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrarTodo(){
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+        
+
+    }
+
+    agregarNumero(numero){
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    imprimirValores(){
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = this.valorAnterior;
+    }
+
+}
